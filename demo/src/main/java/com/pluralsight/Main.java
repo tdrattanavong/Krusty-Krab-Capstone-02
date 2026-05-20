2196514f47f4c23017a811dee0c812a032a0fba5
Taihvonne Rattanavong <tdrattanavong@gmail.com>
2026-05-20 13:37:29 -0700

    Initial commit

---
+++ demo/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class Main {
+    static void main() {
+
+        }
+    }
+
---
+++ demo/src/main/java/com/pluralsight/loops/Person.java
@@ -0,0 +1,22 @@
+package com.pluralsight.loops;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
