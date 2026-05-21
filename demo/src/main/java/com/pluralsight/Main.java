7c25a183fdaed20017c55654a13815f6ceb6f20e
Taihvonne Rattanavong <tdrattanavong@gmail.com>
2026-05-21 14:49:36 -0700

    adding a search text

--- demo/src/main/java/com/pluralsight/Main.java
+++ demo/src/main/java/com/pluralsight/Main.java
@@ -1,8 +1,39 @@
 package com.pluralsight;
 
-public class Main {
-    static void main() {
+import com.pluralsight.loops.Person;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        // Step 1: Create list of people
+        List<Person> people = new ArrayList<>();
+
+        people.add(new Person("Imanuel", "Dartey", 21));
+        people.add(new Person("Yoel", "Weldeselassie", 29));
+        people.add(new Person("Oscar", "Kayaya", 25));
+        people.add(new Person("Jeremy", "Baez", 22));
+        people.add(new Person("Aiden", "Vongkhamchan", 17));
+        people.add(new Person("Caiden", "Seng", 19));
+        people.add(new Person("Taihvonne", "Rattanavong", 21));
+        people.add(new Person("Omar", "Miranda", 24));
+        people.add(new Person("Ariel", "Raymundo", 27));
+        people.add(new Person("Damarrius", "Thompson", 26));
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please type the first or last name you are searching for: ");
+        String name = scanner.nextLine().trim().toLowerCase();
+
+        double age = 0;
+        int count = 0;
+
+        for(Person person : persons){
+            age += person.getAge();
+            count++;
         }
-    }
 
